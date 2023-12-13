3aec40f01aaef4a4974f8f22dd71fd27f2ba1593
aayushk9 <ayushkokate1@gmail.com>
2023-12-14 02:07:50 +0530

    Update Nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,11 @@ const Navbar = () => {
         <input type="text" placeholder="Search..." />
         {/* You can add search icon/button here */}
       </div>
-      <div className="nav-item social-profile">Aayush</div>
+      
+      <div className="nav-item social-profile">
+        <img src="https://th.bing.com/th/id/OIP.JRy2TZsF9d6lFwaROCaj9wHaFA?w=256&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="Profile" className="profile-image" />
+        
+      </div>
     </div>
   );
 };
