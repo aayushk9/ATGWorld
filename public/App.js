de0bfb8869fcd3836c21737ed0d99ec8e4fc1b2a
aayushk9 <ayushkokate1@gmail.com>
2023-12-14 02:55:36 +0530

    add files

---
+++ public/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import PostAction from './PostAction';
+import Create from './Create';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <PostAction />
+      <Create />
+    </div>
+  );
+}
+
+export default App;
+
