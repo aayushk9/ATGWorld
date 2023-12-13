5f4c10554e2c6620890bc491f82a81b683a30f68
aayushk9 <ayushkokate1@gmail.com>
2023-12-13 23:12:20 +0530

    Add src

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import EngineeringComponent from './components/EngineeringComponent';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+    <EngineeringComponent />
+    <NavBar/>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+      <div className="navbar">
+        <ul>
+          <li>All Posts</li>
+          <li>Articles</li>
+          <li>Events</li>
+          <li>Education</li>
+          <li>Job</li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default NavBar;
